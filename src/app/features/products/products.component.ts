78fd8f15d76502e0ad9cf8bbf59d631b7157760e
Cristian Garzon <desarrollo.cristian.garzon@gmail.com>
2024-03-14 17:59:37 -0500

    feat(add): adding products component

---
+++ src/app/features/products/products.component.ts
@@ -0,0 +1,15 @@
+import { Component, inject } from '@angular/core';
+import { CardComponent } from './card/card.component';
+import { ProductService } from '@api/products.service';
+
+@Component({
+  selector: 'app-products',
+  standalone: true,
+  imports: [CardComponent],
+  templateUrl: './products.component.html',
+  styleUrl: './products.component.scss'
+})
+export default class ProductsComponent {
+  private readonly productSvc = inject(ProductService);
+  products = this.productSvc.products;
+}
