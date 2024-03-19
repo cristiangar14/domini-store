ceebaf97e4e7a65db12a2eb9f8003a6cdce3926f
Cristian Garzon <desarrollo.cristian.garzon@gmail.com>
2024-03-19 18:24:14 -0500

    feat(add): adding calculateTotalAmount and calculatePRoductsCount

--- src/app/shared/store/shopping-cart.store.ts
+++ src/app/shared/store/shopping-cart.store.ts
@@ -1,4 +1,5 @@
-import { patchState, signalStore, withMethods, withState } from "@ngrx/signals";
+import { computed } from "@angular/core";
+import { patchState, signalStore, withComputed, withMethods, withState } from "@ngrx/signals";
 import { Product } from "@shared/models/product.interface";
 
 export interface CartStore {
@@ -16,6 +17,10 @@ const initialState: CartStore = {
 export const CartStore = signalStore(
   { providedIn: 'root'},
   withState(initialState),
+  withComputed(({products})=>({
+    productsCount: computed(()=> calculateProductsCount(products())),
+    totalAmount: computed(()=> calculateTotalAmount(products()))
+  })),
   withMethods(
     ({products, ...store}) => ({
       addToCart(product: Product){
@@ -31,3 +36,11 @@ export const CartStore = signalStore(
     })
   ),
 );
+
+function calculateTotalAmount(products: Product[]): number{
+  return products.reduce((acc, product) => acc + product.price, 0);
+};
+
+function calculateProductsCount(products: Product[]): number{
+  return products.reduce((acc, product) => acc + product.qty, 0);
+};
