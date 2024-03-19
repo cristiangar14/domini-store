c20cf3f6a5d4f48703b8cc6433fe0e0cf9147581
Cristian Garzon <desarrollo.cristian.garzon@gmail.com>
2024-03-19 17:47:27 -0500

    feat(add): starting with the ngrxsignal store

---
+++ src/app/shared/store/shopping-cart.store.ts
@@ -0,0 +1,33 @@
+import { patchState, signalStore, withMethods, withState } from "@ngrx/signals";
+import { Product } from "@shared/models/product.interface";
+
+export interface CartStore {
+  products: Product[];
+  totalAmount: number;
+  productsCount: number;
+}
+
+const initialState: CartStore = {
+  products: [],
+  totalAmount: 0,
+  productsCount: 0
+}
+
+export const CartStore = signalStore(
+  { providedIn: 'root'},
+  withState(initialState),
+  withMethods(
+    ({products, ...store}) => ({
+      addToCart(product: Product){
+        patchState(store, {products: [...products(), product]});
+      },
+      removeToCart(id: number){
+        const updateProducts = products().filter(product => product.id != id);
+        patchState(store, {products: updateProducts});
+      },
+      clearCart(){
+        patchState(store, initialState)
+      },
+    })
+  ),
+);
