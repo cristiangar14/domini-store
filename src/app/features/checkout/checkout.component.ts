24ec4c162ad5a9c33bdedd8cdaedc36df00ebf55
Cristian Garzon <desarrollo.cristian.garzon@gmail.com>
2024-03-19 20:12:17 -0500

    feat(add): adding stripe

--- src/app/features/checkout/checkout.component.ts
+++ src/app/features/checkout/checkout.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { CheckoutSvc } from '@api/checkout.service';
+import { CartStore } from '@shared/store/shopping-cart.store';
 
 @Component({
   selector: 'app-checkout',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.scss'
 })
 export default class CheckoutComponent {
+  cartStore = inject(CartStore);
+  private readonly _checkoutService = inject(CheckoutSvc);
 
+  public clearAll(): void{
+    this.cartStore.clearCart();
+  }
+
+  public removeItem(id: number): void {
+    this.cartStore.removeToCart(id);
+  }
+
+  public onProceedToPay(){
+    this._checkoutService.onProceedToPay(this.cartStore.products());
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  apiURL: 'https://fakestoreapi.com'
+  apiURL: 'https://fakestoreapi.com',
+  stripeAPIKey: 'pk_test_51OwCkZ1kA2SSqpYgmVTW1IvsQALqS5hGMPdu9a4d8fWUAtjOA1pfkyfrKnPdTIkGrEylRDcjSY6auUcENve1d3vv000xIyQbk6',
+  serverURL: 'http://localhost:4242'
 };
