481c9847611e92df7f61f562532bf9bd1f32422b
Cristian Garzon <desarrollo.cristian.garzon@gmail.com>
2024-03-19 21:06:42 -0500

    feat(add): adding spinner

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,14 +2,18 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { provideToastr } from 'ngx-toastr';
+import { ErrorResponseInterceptor } from '@shared/interceptors/error-response.interceptor';
+import { SpinnerInterceptor } from '@shared/interceptors/spinner.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimations(),
     provideToastr({timeOut: 900, preventDuplicates: true}),
     provideRouter(routes, withComponentInputBinding()),
-    provideHttpClient(withFetch())
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([ErrorResponseInterceptor, SpinnerInterceptor]))
   ]
 };
